0f00e74c3c3d4eb4456a399422b3eefb85930b09
Koyu Fuke <kouyuu6@gmail.com>
2025-11-03 17:58:28 +0900

    fix: modified lefthook

--- src/schemas/todo_schema.ts
+++ src/schemas/todo_schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod"
 
-export const todoSchema = z.object({
-  title: z.string(),
-  dueDate: z.iso.date().optional(),
-  description: z.string().optional(),
-}).refine((data) => {
+export const todoSchema = z
+  .object({
+    title: z.string(),
+    dueDate: z.iso.date().optional(),
+    description: z.string().optional(),
+  })
+  .refine((data) => {
     return data?.dueDate ? new Date(data?.dueDate) > new Date() : true
-})
+  })
